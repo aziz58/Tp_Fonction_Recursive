52f210f6cec827d933d42220583ecf92846c316d
aziz58 <aziz.algerien58@yahoo.fr>
2017-04-06 21:46:13 +0100

    ajout de style

--- main.c
+++ main.c
@@ -4,10 +4,13 @@
 
 int main(){
 	int n ;
+	printf("====================================================\n");
 	printf("saisissez un nombre pour me servir de tester mes fonctions \n");
+	printf(" number = \n");
 	scanf("%d",	&n);
 	printf("factorielle de %d est : %d\n",n,fact(n));
 	printf("fibonacci(%d) = %d \n",n,fibonacci(n));
 	printf("la somme de tout les entiers entre 1 et %d est : %d\n",n,som(n));
+	printf("====================================================\n");
 	return 0;
 }
